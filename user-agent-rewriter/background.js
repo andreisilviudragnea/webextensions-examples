35d56007b6bf15537f8df7bc129a7820e9713b5f
Andrei Silviu Dragnea <dragnea@adobe.com>
2021-01-28 23:45:38 +0200

    My extension

--- user-agent-rewriter/background.js
+++ user-agent-rewriter/background.js
@@ -1,49 +1,52 @@
 "use strict";
 
-/*
-This is the page for which we want to rewrite the User-Agent header.
-*/
-var targetPage = "https://httpbin.org/*";
+let jsonConfig = {};
 
-/*
-Map browser names to UA strings.
-*/
-var uaStrings = {
-  "Firefox 41": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:41.0) Gecko/20100101 Firefox/41.0",
-  "Chrome 41": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
-  "IE 11": "Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0;  rv:11.0) like Gecko"
-}
-
-/*
-Initialize the UA to Firefox 41.
-*/
-var ua = uaStrings["Firefox 41"];
-
-/*
-Rewrite the User-Agent header to "ua".
-*/
-function rewriteUserAgentHeader(e) {
-  for (var header of e.requestHeaders) {
-    if (header.name.toLowerCase() === "user-agent") {
-      header.value = ua;
+function addOrUpdateHeader(requestHeaders, headerName, headerValue) {
+  for (let header of requestHeaders) {
+    if (header.name.toLowerCase() === headerName) {
+      header.value = headerValue;
+      return;
     }
   }
-  return {requestHeaders: e.requestHeaders};
+  requestHeaders.push({name: headerName, value: headerValue})
 }
 
-/*
-Add rewriteUserAgentHeader as a listener to onBeforeSendHeaders,
-only for the target page.
+function updateHeaders(requestHeaders) {
+  for (let [headerName, headerValue] of Object.entries(jsonConfig)) {
+    addOrUpdateHeader(requestHeaders.requestHeaders, headerName, headerValue);
+  }
+  return {requestHeaders: requestHeaders.requestHeaders};
+}
 
-Make it "blocking" so we can modify the headers.
-*/
-browser.webRequest.onBeforeSendHeaders.addListener(rewriteUserAgentHeader,
-                                          {urls: [targetPage]},
-                                          ["blocking", "requestHeaders"]);
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    updateHeaders,
+    {urls: ["<all_urls>"]},
+    ["blocking", "requestHeaders"]
+);
+
+function updateHeadersToAlter(requestDetails) {
+  console.log("Loading: " + requestDetails.url);
+  let [_, queryString] = requestDetails.url.split("#", 2);
+  for (let pair of queryString.split("&")) {
+    let [key, value] = pair.split("=", 2);
+    if (key === "config") {
+      jsonConfig = JSON.parse(decodeURIComponent(value));
+    }
+  }
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+    updateHeadersToAlter,
+    {urls: ["https://extension.config/*"]},
+    ["blocking"]
+);
+
+let ua = "mere";
 
 /*
 Update ua to a new value, mapped from the uaString parameter.
 */
 function setUaString(uaString) {
-  ua = uaStrings[uaString];
+  ua = uaString;
 }
